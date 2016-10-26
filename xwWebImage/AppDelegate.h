14c9442e5a7bec8f3e82b0925dd91fbee062629c
xuewu <longxuewu@le.com>
2016-10-26 15:20:32 +0800

    Initial Commit

---
+++ xwWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xwWebImage
+//
+//  Created by xuewu.long on 16/10/26.
+//  Copyright © 2016年 xw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xwWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xwWebImage
+//
+//  Created by xuewu.long on 16/10/26.
+//  Copyright © 2016年 xw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
